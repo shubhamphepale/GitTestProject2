38ab755f1190975c6f8723e3e2de7aded27baf60
shubhamphepale <shubhamphepale@gmail.com>
2021-07-16 18:58:49 +0530

    First Commit

---
+++ GitTestProject2/src/com/neosoft/githubtest/GitTest.java
@@ -0,0 +1,16 @@
+package com.neosoft.githubtest;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String code="SE";
+		if(code=="SE") {
+			System.out.println("Software Engineer");
+		}
+		else {
+			System.out.println("Not a Software Engineer");
+		}
+	}
+
+}
